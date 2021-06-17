899a0fa10f12046d585a46c3cbc980e36e8d9429
llanesdavid97 <llanesdavid97@gmail.com>
2021-06-17 12:27:19 -0500

    first commit

---
+++ Application/Fixtures.sql
@@ -0,0 +1,24 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.posts DISABLE TRIGGER ALL;
+
+INSERT INTO public.posts (id, title, body) VALUES ('83822ebb-a401-4b12-81e6-c3a135329cb2', 'Hello World!', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam');
+
+
+ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
---
+++ Web/Controller/Post.hs
@@ -0,0 +1,56 @@
+module Web.Controller.Post where
+
+import Web.Controller.Prelude
+import Web.View.Post.Index
+import Web.View.Post.New
+import Web.View.Post.Edit
+import Web.View.Post.Show
+
+instance Controller PostController where
+    action PostsAction = do
+        post <- query @Post |> fetch
+        render IndexView { .. }
+
+    action NewPostAction = do
+        let post = newRecord
+        render NewView { .. }
+
+    action ShowPostAction { postId } = do
+        post <- fetch postId
+        render ShowView { .. }
+
+    action EditPostAction { postId } = do
+        post <- fetch postId
+        render EditView { .. }
+
+    action UpdatePostAction { postId } = do
+        post <- fetch postId
+        post
+            |> buildPost
+            |> ifValid \case
+                Left post -> render EditView { .. }
+                Right post -> do
+                    post <- post |> updateRecord
+                    setSuccessMessage "Post updated"
+                    redirectTo EditPostAction { .. }
+
+    action CreatePostAction = do
+        let post = newRecord @Post
+        post
+            |> buildPost
+            |> ifValid \case
+                Left post -> render NewView { .. } 
+                Right post -> do
+                    post <- post |> createRecord
+                    setSuccessMessage "Post created"
+                    redirectTo PostsAction
+
+    action DeletePostAction { postId } = do
+        post <- fetch postId
+        deleteRecord post
+        setSuccessMessage "Post deleted"
+        redirectTo PostsAction
+
+buildPost post = post
+    |> fill @["title","body"]
+    |> validateField #title nonEmpty
